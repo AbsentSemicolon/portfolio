0d9da72a8ee1f0aa8b9428e20e4c7b5a1750386c
AbsentSemicolon <1569396+AbsentSemicolon@users.noreply.github.com>
2024-08-30 15:13:07 -0500

    Getting new design implemented

--- src/components/project/project.component.ts
+++ src/components/project/project.component.ts
@@ -2,6 +2,33 @@ import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { LinkComponent } from "../ui/link.component";
 
+interface Language {
+    name: string;
+    version?: string;
+}
+interface Framework {
+    name: string;
+    version: string;
+    link: string;
+}
+interface ThirdParty {
+    name: string;
+    link?: string;
+}
+
+interface ProjectLink {
+    name: string;
+    link: string;
+}
+interface Project {
+    name: string;
+    link: string;
+    languageList: Language[];
+    frameworkList: Framework[];
+    github: string;
+    thirdPartyList: ThirdParty[];
+    linkList: ProjectLink[];
+}
 @Component({
     selector: "app-project",
     standalone: true,
@@ -10,28 +37,82 @@ import { LinkComponent } from "../ui/link.component";
     styleUrl: "./project.component.scss"
 })
 export class ProjectComponent {
-    masonryOptions = {};
-    projects = [
+    projectList: Project[] = [
         {
             name: "Application Tracker",
             link: "https://missingsemicolon.net/tracker?demo=true",
-            languageList: ["JavaScript/TypeScript"],
-            frameworkList: ["React(v18)", "Tailwind"],
-            github: "apply-tracker"
+            languageList: [
+                {
+                    name: "JavaScript"
+                },
+                { name: "TypeScript" }
+            ],
+            frameworkList: [
+                {
+                    name: "React",
+                    version: "18",
+                    link: ""
+                }
+            ],
+            github: "apply-tracker",
+            thirdPartyList: [
+                {
+                    name: "Tailwind"
+                },
+                {
+                    name: "react-responsive-masonry",
+                    link: "https://www.npmjs.com/package/react-responsive-masonry"
+                },
+                {
+                    name: "react-google-charts",
+                    link: "https://www.react-google-charts.com/"
+                }
+            ],
+            linkList: []
         },
         {
             name: "Portfolio",
             link: "",
-            languageList: ["JavaScript/TypeScript"],
-            frameworkList: ["Angular(v18.1.0)", "SASS"],
-            github: "private"
+            languageList: [
+                {
+                    name: "JavaScript"
+                },
+                { name: "TypeScript" }
+            ],
+            frameworkList: [
+                {
+                    name: "Angular",
+                    version: "18.1.0",
+                    link: ""
+                }
+            ],
+            github: "private",
+            thirdPartyList: [
+                {
+                    name: "SASS"
+                },
+                {
+                    name: "Google Fonts"
+                },
+                {
+                    name: "Google Icons"
+                }
+            ],
+            linkList: [
+                {
+                    name: "Figma",
+                    link: "https://www.figma.com/design/t4Jx3mmAaM0w04I3wuAdvv/Portfolio?node-id=0-1&t=deL611hzNibUxJdc-1"
+                }
+            ]
         },
         {
             name: "Remnant: From the Files",
             link: "",
-            languageList: ["Python"],
+            languageList: [{ name: "Python", version: "3.x.x" }],
             frameworkList: [],
-            github: "remnant_from_file_files"
+            github: "remnant_from_file_files",
+            thirdPartyList: [],
+            linkList: []
         }
     ];
 }
