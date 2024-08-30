983aaa955e4c132380cbc7a4784e6274dc7b61fd
AbsentSemicolon <1569396+AbsentSemicolon@users.noreply.github.com>
2024-08-30 14:13:21 -0500

    Setting up link component

---
+++ src/components/ui/link.component.ts
@@ -0,0 +1,29 @@
+/* Making links easier/consistent to use.
+ *
+ * Usage:
+ *  <app-link
+ *   linkUrl="<url with protocol>"
+ *   linkText="<text to be displayed>"
+ *   external="true|false"
+ *  ></app-link>
+ *
+ * Setting external to true with show icon indicating
+ * link will be taking user away from site.
+ */
+
+import { Component, Input, booleanAttribute } from "@angular/core";
+
+import { CommonModule } from "@angular/common";
+
+@Component({
+    selector: "app-link",
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: "./link.component.html",
+    styleUrl: "./link.component.scss"
+})
+export class LinkComponent {
+    @Input() linkUrl: string = "";
+    @Input({ required: true }) linkText: string = "";
+    @Input({ transform: booleanAttribute }) external: boolean = false;
+}
