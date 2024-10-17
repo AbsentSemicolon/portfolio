15221ec03daf8039d64489df22541058978ccfc8
AbsentSemicolon <1569396+AbsentSemicolon@users.noreply.github.com>
2024-10-17 16:00:42 -0500

    Configuring tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./src/**/*.{html,ts}",];
+export const theme = {
+    extend: {},
+    colors: {
+        "link": "#ff69de"
+    }
+};
+export const plugins = [];
+
